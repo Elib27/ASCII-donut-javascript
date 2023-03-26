3ce56049ce48e4cf19b625a1162969de7bc16842
Eliot Bas <eliot.bas@orange.fr>
2023-03-26 23:02:34 +0200

    finished animation

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 // Donut Javascript Code inspired from https://www.a1k0n.net/2006/09/15/obfuscated-c-donut.html
 
 
-const screen_size = 100 // height and width of projection screen
+const screen_size = 50 // height and width of projection screen
 
 const R1 = 1 // radius of the tube (tore) 
 const R2 = 2 // radius of the tore minus 2*R1
-const K2 = 6 // distance between the projection screen and the tore
+const K2 = 5 // distance between the projection screen and the tore
 const K1 = screen_size * K2 * 3 / (8 * (R1 + R2)) // distance between the camera and the projection screen
 
-const THETA_STEP = 0.2
-const PHI_STEP = 0.1
+const THETA_STEP = 0.02
+const PHI_STEP = 0.01
 
-const canvas = document.querySelector("#render-canvas");
-const ctx = canvas.getContext("2d");
-ctx.font = '12px monospace';
-
+const renderContainer = document.querySelector("#render-div")
 
 function animateDonut(A, B) {
-  
-  const output = Array(screen_size).fill(Array(screen_size).fill(' '))
-  const zBuffer = Array(screen_size).fill(Array(screen_size).fill(0))
+
+  const output = Array.from({length: screen_size}, () => Array.from({length: screen_size}, () => '&nbsp;'))
+  const zBuffer = Array.from({length: screen_size}, () => Array.from({length: screen_size}, () => 0))
 
   const cosA = Math.cos(A)
   const sinA = Math.sin(A)
@@ -33,6 +30,7 @@ function animateDonut(A, B) {
     {
       const cosPhi = Math.cos(phi)
       const sinPhi = Math.sin(phi)
+
       // point X(x,y,z) of the tore
       const x = (R2 + R1*cosTheta)*(cosB*cosPhi + sinA*sinB*sinPhi) - R1*cosA*sinB*sinTheta
       const y = (R2 + R1*cosTheta)*(cosPhi*sinB - cosB*sinA*sinPhi) + R1*cosA*cosB*sinTheta
@@ -48,27 +46,26 @@ function animateDonut(A, B) {
 
       // Z-buffer
       if (1/z < zBuffer[yProj][xProj]) continue
-
       zBuffer[yProj][xProj] = 1/z
       const luminanceIndex = Math.round(L*8) 
       output[yProj][xProj] = ".,-~:;=!*#$@"[luminanceIndex]
     }
   }
-
-  // Draw in the canvas
-  // for (let i = 0; i < screen_size; i++)
-  // {
-  //   console.log(output[i].join(''))
-  // }
-  console.log(output)
+  // Print ASCII
+  let ASCII_result = ""
+  for (let i = 0; i < screen_size ; i++) {
+    ASCII_result += output[i].join('') + "<br>"
+  }
+  renderContainer.innerHTML = ASCII_result
 }
 
 
-
+// Animation
 let i = 0
+const STEP = 0.05
+const frequency = 25
 
 setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   animateDonut(i, i/2)
-  i+=0.05
-}, 1000/1)
+  i += STEP
+}, 1000/frequency)
