3adc6cd96e88772a173aca6ab4bdb6d2056e20d5
Eliot Bas <eliot.bas@orange.fr>
2023-03-29 18:27:20 +0200

    add cone

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const R1 = 1 // radius of the tube (tore)
 const R2 = 2 // radius of the tore (minus 2*R1)
 const Zoff = 8 // distance between the projection screen and the object
 const K1 = screen_size * Zoff * 3 / (8 * (R1 + R2)) // distance between the camera and the projection screen
+
+// Torus parameters
 const THETA_STEP = 0.02
 const PHI_STEP = 0.01
+
+// Cube parameters
 const CUBE_SIZE = 4
 const CUBE_STEP = 0.05
 
 const renderContainer = document.querySelector("#render-div")
 const shapeInputs = document.querySelectorAll('input[name="shape"]');
 
-let shape = "torus"
+let shape = "cone"
 
 shapeInputs.forEach(input => input.addEventListener("click", () => (shape = input.value)))
 
@@ -31,7 +35,7 @@ setInterval(() => {
       animateCube(animProgress, animProgress/2)
       break;
     default: 
-      animateDonut(animProgress, animProgress/2)
+      animateCone(animProgress, animProgress/4)
   }
   animProgress += STEP
 }, 1000/frequency)
@@ -53,7 +57,7 @@ function animateDonut(A, B) {
       const cosPhi = Math.cos(phi)
       const sinPhi = Math.sin(phi)
 
-      // point of the tore
+      // animeted point of the tore
       const x = (R2 + R1*cosTheta)*(cosB*cosPhi + sinA*sinB*sinPhi) - R1*cosA*sinB*sinTheta
       const y = (R2 + R1*cosTheta)*(cosPhi*sinB - cosB*sinA*sinPhi) + R1*cosA*cosB*sinTheta
       const z = Zoff + cosA*(R2 + R1*cosTheta)*sinPhi + R1*sinA*sinTheta
@@ -156,6 +160,90 @@ function animateCube(A, B) {
         zBuffer[yProj][xProj] = 1/z
         // const luminanceIndex = Math.round(L*8) 
         const luminanceIndex = 0
+        output[yProj][xProj] = ".,-~:;=!*#$@"[luminanceIndex]
+      }
+    }
+  }
+  // Print ASCII in the DOM
+  let ASCII_result = ""
+  for (let i = 0; i < screen_size ; i++) {
+    ASCII_result += output[i].join('') + "<br>"
+  }
+  renderContainer.innerHTML = ASCII_result
+}
+
+// Cone parameters
+CONE_HEIGHT = 3
+CONE_RADIUS = 1.5
+CONE_ANGLE_STEP = 0.02
+CONE_RADIUS_STEP = 0.02
+CONE_HEIGHT_STEP = 0.02
+
+function animateCone(A, B) { //
+
+  const output = Array.from({length: screen_size}, () => Array.from({length: screen_size}, () => '&nbsp;'))
+  const zBuffer = Array.from({length: screen_size}, () => Array.from({length: screen_size}, () => 0))
+
+  const cosA = Math.cos(A)
+  const sinA = Math.sin(A)
+  const cosB = Math.cos(B)
+  const sinB = Math.sin(B)
+
+  for (let theta = 0; theta < 2 * Math.PI; theta += CONE_ANGLE_STEP) {
+    const cosTheta = Math.cos(theta)
+    const sinTheta = Math.sin(theta)
+    for (let h = 0; h < CONE_HEIGHT; h += CONE_HEIGHT_STEP) {
+
+      if (h === 0) { // on the cone base
+        for (let r = 0; r < CONE_RADIUS ; r += CONE_RADIUS_STEP) {
+          // point of the static cone
+          const xs = r*cosTheta
+          const ys = - CONE_HEIGHT / 2
+          const zs = r*sinTheta
+
+          // point of the animated cone
+          const x = cosB*xs + sinB*ys
+          const y = -cosA*sinB*xs + cosA*cosB*ys + sinA*zs
+          const z = Zoff + sinA*sinB*xs - cosB*sinA*ys + cosA*zs
+
+          // screen projection
+          const xProj = Math.round(screen_size / 2 + K1 * x / z)
+          const yProj = Math.round(screen_size / 2 - K1 * y / z)
+
+          // Luminance with light vector: (0, 1, -1)
+          const L = -cosA*cosB
+          if (-L < 0) continue
+          // Z-buffer
+          if (1/z < zBuffer[yProj][xProj]) continue
+          zBuffer[yProj][xProj] = 1/z
+          const luminanceIndex = Math.round(-L*8) 
+          // const luminanceIndex = 0
+          output[yProj][xProj] = ".,-~:;=!*#$@"[luminanceIndex]
+        }
+      }
+      else {
+        // point of the tore statique cone
+        const xs = cosTheta*(CONE_RADIUS - CONE_RADIUS*h / CONE_HEIGHT)
+        const ys = h - CONE_HEIGHT / 2
+        const zs = sinTheta*(CONE_RADIUS - CONE_RADIUS*h / CONE_HEIGHT)
+  
+        // point on the animated cone
+        const x = cosB*xs + sinB*ys
+        const y = -cosA*sinB*xs + cosA*cosB*ys + sinA*zs
+        const z = Zoff + sinA*sinB*xs - cosB*sinA*ys + cosA*zs
+  
+        // screen projection
+        const xProj = Math.round(screen_size / 2 + K1 * x / z)
+        const yProj = Math.round(screen_size / 2 - K1 * y / z)
+  
+        // Luminance with light vector: (0, 1, -1)
+        const L = sinB*cosTheta + sinTheta*(cosA*cosB - sinA) - Math.sin(Math.atan(CONE_RADIUS/CONE_HEIGHT))*(sinA*cosB +cosA)
+        if (-L < 0) continue
+        // Z-buffer
+        if (1/z < zBuffer[yProj][xProj]) continue
+        zBuffer[yProj][xProj] = 1/z
+        const luminanceIndex = Math.round(-L*8) 
+        // const luminanceIndex = 0
         output[yProj][xProj] = ".,-~:;=!*#$@"[luminanceIndex]
       }
     }
